ded874ca87f54cf79de4fcfe3fe9190addcb60a0
cruzrobin <45374883+cruzrobin@users.noreply.github.com>
2018-11-27 11:31:52 +0800

    Update acitivity_b_cruz.py

--- acitivity_b_cruz.py
+++ acitivity_b_cruz.py
@@ -2,15 +2,15 @@ for x in range(8):
     print(2**x, end = '\t')
 print()
 
-for y in range(1,13):
-    if y % 4 == 0:
+for x in range(1,13):
+    if x % 4 == 0:
         print("S", end = '\t')
     else:
         print(x, end = '\t')
 print()
 
-for z in range(1, 9):
-    if z % 8 == 1:
+for x in range(1, 9):
+    if x % 8 == 1:
         print(x, end = '\t')
     else:
         print("E", end = '\t')
