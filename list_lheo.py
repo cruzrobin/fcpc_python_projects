8d33de65b2356df4fdeeb1d0fb8aad6c83948b1e
cruzrobin <45374883+cruzrobin@users.noreply.github.com>
2018-11-27 10:02:33 +0800

    Create list_lheo.py

---
+++ list_lheo.py
@@ -0,0 +1,17 @@
+#String
+mga_kaibigan = ["jerome", "arvi", "ralph", "mark"]
+mga_numbers = [10,20,30,40]
+mga_decimal = [1.0,1.2,1.3,1.4]
+
+for x in range (len(mga_kaibigan)):
+    print(mga_kaibigan[x])
+
+for y in range (len(mga_numbers)):
+        print(mga_numbers[y])
+
+for z in  range (len(mga_decimal)):
+    print(mga_decimal[z])
+
+print(len(mga_kaibigan))
+print(len(mga_numbers))
+print(len(mga_decimal))
